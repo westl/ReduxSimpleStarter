0f14c0e08babb831af4a2b0e094f4e4023030a21
lamar west <westl@uci.edu>
2018-02-25 20:28:17 -0800

    added more properties to the book object

--- src/containers/book-details.js
+++ src/containers/book-details.js
@@ -13,7 +13,8 @@ class BookDetail extends Component {
         return (
             <div>
                 <h3>Details for:</h3>
-                <div>{this.props.activeBook.title}</div>
+                <div>Title: {this.props.activeBook.title}</div>
+                <div>Pages: {this.props.activeBook.pages}</div>
             </div>
         )
     }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,12 +1,15 @@
 export default function () {
     return [{
-        title: 'Good Data Is Good For Testing'
+        title: 'Good Data Is Good For Testing',
+        pages: 5
     },
     {
-        title: 'Javascript, The Good Parts'
+        title: 'Javascript, The Good Parts',
+        pages: 10
     },
     {
-        title: 'Lamar Learns Redux'
+        title: 'Lamar Learns Redux',
+        pages: 1
     }];
 }
 

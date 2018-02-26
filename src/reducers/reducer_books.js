61b73f5513da98e784399e98a9979fb9eb0dc267
lamar west <westl@uci.edu>
2018-02-25 18:47:43 -0800

    created reducer for books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [{
+        title:'Javascript, The Good Parts',
+        title:'Lamar Learns Redux',
+        title:'Good Data Is Good For Testing'
+    }];
+}
+
